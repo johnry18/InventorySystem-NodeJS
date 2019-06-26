a0609b4d787da187274eac0f747e7a6d491af3a2
Johnry Bautista <johnrybautista18@gmail.com>
2019-06-27 01:31:07 +0800

    Use online database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb://localhost:27017/LoginSystem'
+dbPassword = 'mongodb://db-testingnodejs:EQ395MdhYdhyD2ODZysWE6j7ZUGW3v89wCpCY8GFrpI2ca7BhmvvPYjFOnvOQEbH8XmP3WVVFUlF8xco9UIXew==@db-testingnodejs.documents.azure.com:10255/inventorysystem?ssl=true&replicaSet=globaldb'
 module.exports = {
     mongoURI: dbPassword
 };
